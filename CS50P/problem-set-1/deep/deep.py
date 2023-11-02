852ec19441296c2bcda379704e0288d133701a18
slah e thaer <salah.e.thaer@gmail.com>
2023-11-02 18:08:21 +0200

    Solving 'deep' problem

---
+++ CS50P/problem-set-1/deep/deep.py
@@ -0,0 +1,6 @@
+deep = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+
+if deep == "42" or deep == "forty-two" or deep == "Forty Two" :
+    print("Yes")
+else:
+    print("No")
