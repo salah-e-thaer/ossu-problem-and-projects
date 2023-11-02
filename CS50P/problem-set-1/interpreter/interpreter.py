999f0196dba693b815034722ab39bcd58c335af3
slah e thaer <salah.e.thaer@gmail.com>
2023-11-03 01:59:30 +0200

    Solve 'extensions' and 'interpreter' problems

---
+++ CS50P/problem-set-1/interpreter/interpreter.py
@@ -0,0 +1,17 @@
+expression = input("Expression: ")
+
+x, y, z = expression.split(" ")
+
+x = int(x)
+z = int(z)
+
+if y == "+":
+    print(float(x + z))
+elif y == "-":
+    print(float(x - z))
+elif y == "*":
+    print(float(x * z))
+elif y == "/":
+    print(float(x / z))
+else:
+    print("please, enter correct expression.")
